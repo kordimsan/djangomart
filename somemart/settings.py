39f72ad5161dfe4161067afc7e4601a19ee49f4b
Dmitry Korzunin <kordimsan@mail.ru>
2021-03-17 19:30:28 +0500

    hide secrets

--- somemart/settings.py
+++ somemart/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ln%**8s&*g!gba@hoh&^bk6=aa1h9(&8$v_d2^c*h&nvo&pe&3'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
